5ec6abd3a8d96b2db9883a76dd67a7f399885d46
Vathys <deepdesai00@gmail.com>
2026-01-21 20:28:50 -0600

    style: Moved to src/ folder and aliased imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import "./global.css";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import { Navbar } from "./components/nav";
-import Footer from "./components/footer";
-import { baseUrl } from "./sitemap";
+import { Navbar } from "@/nav";
+import Footer from "@/footer";
+import { baseUrl } from "@/sitemap";
+import "@/global.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
